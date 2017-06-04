db4bb6137f10e17b319250e0b682a418f2e7bbd1
alkahan <alkahan@free.fr>
2017-06-04 11:01:51 +0200

    maj script de publication

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 
+git checkout -b blog
 hugo -t blog
+git add -f public
+git commit -m "publication"
 git subtree push --prefix=public git@github.com:alkahan/blog.git gh-pages --squash
+git checkout master
